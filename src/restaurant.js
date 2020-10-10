5db1ca69150c23574d58193a486bf9b20035bf8f
Max Bregman <bregmanmax91@gmail.com>
2020-10-10 15:38:55 -0600

    make addMenuItem function

--- src/restaurant.js
+++ src/restaurant.js
@@ -2,9 +2,20 @@ function createRestaurant(name) {
   return {name: name, menus: {breakfast: [], lunch: [], dinner: []}};
 }
 
+function addMenuItem(establishment, item) {
+  if (item.type === 'lunch' && item != establishment.menus.lunch[0]) {
+    establishment.menus.lunch.push(item);
+  }
+  if (item.type === 'breakfast' && item != establishment.menus.breakfast[0]) {
+    establishment.menus.breakfast.push(item);
+  }
+  if (item.type === 'dinner' && item != establishment.menus.dinner[0]) {
+    establishment.menus.dinner.push(item);
+  }
+}
 
 module.exports = {
   createRestaurant,
-//   addMenuItem,
+  addMenuItem,
 //   removeMenuItem
  }
