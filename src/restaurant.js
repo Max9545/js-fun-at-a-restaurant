1c85c41c5644604bd5255d10220d1e86941a9d82
Max Bregman <bregmanmax91@gmail.com>
2020-10-10 12:39:48 -0600

    make createRestaurant function

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,10 @@
+function createRestaurant(name) {
+  return {name: name, menus: {breakfast: [], lunch: [], dinner: []}};
+}
 
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
-}
+  createRestaurant,
+//   addMenuItem,
+//   removeMenuItem
+ }
