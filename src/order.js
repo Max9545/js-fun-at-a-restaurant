e3c522bed30cd121aab620ba4cce94b7421a144c
Max Bregman <bregmanmax91@gmail.com>
2020-10-10 12:21:25 -0600

    make takeOrder function

--- src/order.js
+++ src/order.js
@@ -1,7 +1,15 @@
+function takeOrder(foodOrder, list) {
+  if (list.length < 3) {
+    list.push(foodOrder);
+  }
+}
+
+
+
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+   takeOrder,
+   // refundOrder,
+   // listItems,
+   // searchOrder
 }
