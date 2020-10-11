f759b79a56dbc8c9ec988b4304cfb4c72764c444
Max Bregman <bregmanmax91@gmail.com>
2020-10-11 13:15:09 -0600

    make checkForFoodr method

--- src/Chef.js
+++ src/Chef.js
@@ -9,6 +9,26 @@ class Chef {
     }
     return `Hello, ${name}!`;
   }
+  checkForFood(browseItem) {
+   for (var i = 0; i < this.restaurant.menus.breakfast.length; i++) {
+     if (this.restaurant.menus.breakfast[i].name === browseItem.name) {
+       return `Yes, we're serving ${browseItem.name} today!`;
+     }
+   }
+   for (var i = 0; i < this.restaurant.menus.lunch.length; i++) {
+     if (this.restaurant.menus.lunch[i].name === browseItem.name) {
+       return `Yes, we're serving ${browseItem.name} today!`;
+     }
+   }
+   for (var i = 0; i < this.restaurant.menus.dinner.length; i++) {
+     if (this.restaurant.menus.dinner[i].name === browseItem.name) {
+       return `Yes, we're serving ${browseItem.name} today!`;
+     }
+   }
+   return `Sorry, we aren't serving ${browseItem.name} today.`;
+ }
+
+
 }
 
 
